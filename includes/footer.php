8aa8f868a900d1ad8b1e6fd2c298d221df48c1cd
Nancy Brar <nbrar@algomau.ca>
2024-06-27 22:36:50 -0400

    Lab7 Completed.....

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+<footer
+style=
+"text-align: center;
+padding: 10px 0; 
+position: relative; 
+bottom: 0; 
+width: 100%;">
+    &copy; 2024 My Pets. All rights reserved.
+</footer>
+</body>
+</html>
---
+++ php/behavior.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Behavior</title>
+    <link rel="stylesheet" href="../css/bootstrap.css" />
+    <link rel="stylesheet" href="../css/style.css" />
+  </head>
+  <body>
+
+    <?php
+    include '../includes/header.php';
+    ?>
+
+<main>
+    <h1>Welcome to the Animal Behavior Page!</h1>
+    <p>
+        Here, we delve into the fascinating world of animals and their unique behaviors.
+        Discover how different species interact with their environment, communicate with each other,
+        and adapt to challenges through instinctual or learned behaviors.
+        Explore articles on social structures within animal communities, mating rituals, hunting strategies,
+        and much more. Our goal is to provide insightful information that helps you understand and appreciate
+        the complexity of animal life.
+    </p>
+</main>
+
+    <script
+    src="../js/bootstrap.bundle.min.js">
+    </script>
+
+    <script
+    src="../js/darkmodetoggle.js">
+    </script>
+
+    <script
+    src="../js/script.js">
+    </script>
+
+    <?php
+    include '../includes/footer.php';
+    ?>
+
+    </body>
+</html>
+  
